b01da703d58e296f1888ef9558517f0d55f65318
[your <rifai.it@gmail.com>
2019-03-18 14:20:47 +0100

    main.js ex1

---
+++ main.js
@@ -0,0 +1,25 @@
+
+console.log("this is an exercise in console logging");
+var myValue="This is the value.";
+console.log(myValue);
+var firstName="Yaser ",lastName=" Rifai";
+console.log("My Name is "+firstName+lastName);
+
+var fName="Samer",lName="Ahmad",age=40,city="Berlin",job="Doctor";
+console.log("My Full Name is "+fName+" "+lName+
+" and  My Age is "+age+" and My City is "+city+" and My job is "+job);
+
+console.log(fName+" "+lName+" is a "+
+age+" year old "+job+ " who live in "+city);
+
+
+
+
+
+
+
+
+
+
+
+
